1a8086c43d227e24fd447888d086f09e6984ed19
JoyWanddrr <m81y102@msn.com>
2022-11-10 15:16:51 +0800

    feat:create models seed

---
+++ models/item.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+  itemName: { type: String, require: true },
+  category: { type: String, require: true },
+  date: { type: String, require: true },
+  amount: { type: Number, require: true },
+  icon: { type: String, require: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', index: true, required: true }
+})
+
+module.exports = mongoose.model('Item', itemSchema)
